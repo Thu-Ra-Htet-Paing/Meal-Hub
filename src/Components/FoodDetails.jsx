40eb6cb2df3941345492c8be7f0994eb8aa56edf
Jad Jad <dumbicjad@gmail.com>
2025-10-25 15:54:27 +0630

    Price per serving fixed

--- src/Components/FoodDetails.jsx
+++ src/Components/FoodDetails.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import styles from './fooddetails.module.css';
-import IngredientList from './IngredientList';
+import React, { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css";
+import IngredientList from "./IngredientList";
 
-function FoodDetails( {foodId} ) {
+function FoodDetails({ foodId }) {
   const [recipe, setRecipe] = useState({});
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
-  const API_KEY = `adc200bcd89a47138f3bf0d59a3a59f8`;
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
-    async function fetchFood () {
+    async function fetchFood() {
       const respond = await fetch(`${URL}?apiKey=${API_KEY}`);
       const data = await respond.json();
-      console.log(data);
       setRecipe(data);
       setIsLoading(false);
     }
@@ -23,7 +22,7 @@ function FoodDetails( {foodId} ) {
     <div>
       <div className={styles.recipeCard}>
         <h1 className={styles.title}>{recipe.title}</h1>
-        <img className={styles.image} src={recipe.image} alt='Food Pic'></img>
+        <img className={styles.image} src={recipe.image} alt="Food Pic"></img>
 
         <div className={styles.details}>
           <span>
@@ -35,42 +34,46 @@ function FoodDetails( {foodId} ) {
           </span>
 
           <span>
-            <strong>{recipe.vegetarian ? '🍅 Vegetarian' : '🍖 Non-Vegetarian'}</strong>
+            <strong>
+              {recipe.vegetarian ? "🍅 Vegetarian" : "🍖 Non-Vegetarian"}
+            </strong>
           </span>
 
           <span>
-            <strong>{recipe.vegan ? '🐮 Vegan' : ''}</strong>
+            <strong>{recipe.vegan ? "🐮 Vegan" : ""}</strong>
           </span>
         </div>
 
         <div>
           <span>
-            <strong>💲{recipe.pricePerServing/100} Per Serving</strong>
+            <strong>
+              💲{(recipe.pricePerServing / 100).toFixed(2)} Per Serving
+            </strong>
           </span>
         </div>
 
         <h2>Ingredients</h2>
-        {
-          isLoading ? (<p className={styles.p}>Loading...</p>) : <IngredientList recipe={recipe}/>
-        }
-
-
+        {isLoading ? (
+          <p className={styles.p}>Loading...</p>
+        ) : (
+          <IngredientList recipe={recipe} />
+        )}
 
         <h2>Instructions</h2>
         <div className={styles.instructions}>
           <ol>
-          {
-            isLoading? 
-            (<p>Loading...</p>) : 
-            recipe.analyzedInstructions[0].steps.map(step => (<li key={step.number}>{step.step}</li>))
-          }
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              recipe.analyzedInstructions[0].steps.map((step) => (
+                <li key={step.number}>{step.step}</li>
+              ))
+            )}
           </ol>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default FoodDetails
-
-
+export default FoodDetails;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import styles from './search.module.css';
+import React, { useEffect, useState } from "react";
+import styles from "./search.module.css";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
-const API_KEY = "adc200bcd89a47138f3bf0d59a3a59f8";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-function SearchBar({foodData, setFoodData}) {
-    const [search, setSearch] = useState("pizza");
+function SearchBar({ foodData, setFoodData }) {
+  const [search, setSearch] = useState("pizza");
 
-    useEffect(()=>{
-        async function fetchFood() {
-            const respond = await fetch(`${URL}?query=${search}&apiKey=${API_KEY}`);
-            const data = await respond.json();
-            console.log(data.results)
-            setFoodData(data.results)
-        }
-        fetchFood()
-    },[search])
+  useEffect(() => {
+    async function fetchFood() {
+      const respond = await fetch(`${URL}?query=${search}&apiKey=${API_KEY}`);
+      const data = await respond.json();
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [search]);
 
   return (
     <div className={styles.SearchBar}>
-      <input type='text' value={search} onChange={(e)=>setSearch(e.target.value)} className={styles.input}></input>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className={styles.input}
+      ></input>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
